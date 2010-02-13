2ed77405b33466e23aeb10967c75109eeaabb069
Stephen Krol <krol17@gmail.com>
2010-02-13 15:22:29 -0500

    test

---
+++ src/android/apps/MobileTask.java
@@ -0,0 +1,13 @@
+package android.apps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MobileTask extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
